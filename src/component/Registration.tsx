bbb95950e6ee15a2ab0ec37fb23a26e0bf567ce8
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-07-14 09:57:29 +0900

    Feat : 객체로 분리한 부분 적용

--- src/component/Registration.tsx
+++ src/component/Registration.tsx
@@ -1,3 +1,4 @@
+import { validationRules } from "../data/validationRules";
 import useForm from "../hooks/useForm";
 
 import { useEffect, useState } from "react";
@@ -10,41 +11,13 @@ interface User {
 }
 
 const Registration = () => {
+  const [isValid, setIsValid] = useState(false);
   const {
     handleSubmit,
     handleChange,
     data: user,
     errors,
-  } = useForm<User>({
-    validations: {
-      name: {
-        pattern: {
-          value: /^[A-Za-z]+$/,
-          message: "이름에 특수문자는 허용되지 않습니다.",
-        },
-      },
-      age: {
-        custom: {
-          isValid: (value) => Number(value) > 17,
-          message: "나이는 17보다 커야 합니다.",
-        },
-      },
-      password: {
-        custom: {
-          isValid: (value) => value.length > 6,
-          message: "비밀번호는 6자 이상이어야 합니다.",
-        },
-      },
-      email: {
-        pattern: {
-          value: /^[A-Za-z0-9]+@[a-z]+\.[a-z]{2,3}$/,
-          message: "유효한 이메일을 입력해주세요.",
-        },
-      },
-    },
-  });
-
-  const [isValid, setIsValid] = useState(false);
+  } = useForm<User>({ validations: validationRules });
 
   const register = (key: keyof User) => ({
     name: key,
